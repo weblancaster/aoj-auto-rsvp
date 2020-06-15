1928b3160086345acbf05bed5e363e06afef61c3
Michael Lancaster <michaell.llancaster@gmail.com>
2020-06-15 02:06:36 -0700

    login and goto aoj classes list

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ let browserPromise = puppeteer.launch({
   args: ["--no-sandbox"],
 });
 
-exports.aojAutoRSVP = async (req, res) => {
-  const url = "https://mindbody.io/locations/art-of-jiu-jitsu";
+const loginPage = "https://mindbody.io/login";
+const aojPage = "https://mindbody.io/locations/art-of-jiu-jitsu";
+const loginRequest = "https://identity.mparticle.com/v1/login";
 
+exports.aojAutoRSVP = async (req, res) => {
   const browser = await browserPromise;
   const context = await browser.createIncognitoBrowserContext();
   const page = await context.newPage();
 
-  await page.goto(url);
+  await page.goto(loginPage, {
+    waitUntil: "load",
+  });
+
+  // login
+  await page.type('input[name="email"]', process.env.CRED_EMAIL);
+  await page.type('input[name="password"]', process.env.CRED_PWD);
+  await page.keyboard.press("Enter");
+  await page.waitForResponse(loginRequest);
+
+  // go to aoj page
+  await page.goto(aojPage);
+  await page.waitFor(5000);
+
+  // select class
 
   const image = await page.screenshot({ fullPage: true });
 
